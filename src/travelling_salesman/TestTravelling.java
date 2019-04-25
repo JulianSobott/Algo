c07e9b708d5bb609129714b36db9671b46996460
JulianSobott <julian.sobott@gmail.com>
2019-04-25 10:52:24 +0200

    Initial commit

---
+++ src/travelling_salesman/TestTravelling.java
@@ -0,0 +1,28 @@
+package travelling_salesman;
+
+import travelling_salesman.greedy.GreedyAlgorithm;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestTravelling {
+
+    @org.junit.jupiter.api.Test
+    void test_greedy(){
+        String filePath = "Resources/distances.txt";
+        int[] route = GreedyAlgorithm.solve(filePath, 0, 4);
+        int[] expected = {0, 2, 1, 3, 0};
+        assertArrayEquals(route, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    void test_greedy_all(){
+        String filePath = "Resources/distances.txt";
+        int[] route = GreedyAlgorithm.solve(filePath, 0, -1);
+        System.out.println(Arrays.toString(route));
+    }
+}
